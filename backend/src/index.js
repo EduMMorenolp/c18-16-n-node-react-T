ab3fa8d46a44a748b5dc941f9bd5b7c0dac8b37a
Edu Moreno <e.m.morenolp@gmail.com>
2024-05-22 00:41:06 -0300

    add morgan

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 const authLoginRoutes = require('./routes/authLoginRoutes');
 
 const app = express();
 const portLocal = 3000;
 const PORT = process.env.PORT || portLocal;
 
+app.use(morgan('dev')); // Middleware de Morgan para registro de solicitudes HTTP
 app.use(express.json());
 
 // Rutas de autenticación
