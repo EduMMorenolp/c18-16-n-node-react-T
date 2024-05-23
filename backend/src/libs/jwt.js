0795554882465bae3b4a32424b786d7351c9e2a1
Edu Moreno <e.m.morenolp@gmail.com>
2024-05-23 00:27:32 -0300

    fix registro token

--- backend/src/libs/jwt.js
+++ backend/src/libs/jwt.js
@@ -1,8 +1,9 @@
 const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = process.env;
+require('dotenv').config();
 
 const createAccessToken = (payload) => {
     return new Promise((resolve, reject) => {
+        const JWT_SECRET = process.env.JWT_SECRET;
         jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' }, (err, token) => {
             if (err) reject(err);
             resolve(token);
