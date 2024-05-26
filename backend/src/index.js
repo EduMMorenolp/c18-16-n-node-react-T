29128e4a87c221e10fb9925e97ed256c0cd0ab39
Edu Moreno <e.m.morenolp@gmail.com>
2024-05-26 00:59:59 -0300

    fix errores de merge

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const auths = require('./routes/auths.routes')
 const roles = require('./routes/roles.routes')
+const teacher = require('./routes/teacher.Routes')
 require('dotenv').config()
 
 const app = express()
@@ -14,7 +15,7 @@ app.use(express.json())
 app.use('/api/auth', auths)
 app.use('/api', roles)
 // Rutas de profesor y cursos
-app.use('/api', teacherRoutes);
+app.use('/api', teacher);
 
 app.listen(PORT, () => {
   console.log('\n==================================================')
--- backend/src/routes/auth.Routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { iniciarSesion, registrarUsuario } = require('../controllers/authController')
-
-// Ruta para iniciar sesión
-router.post('/login', iniciarSesion)
-router.post('/register', registrarUsuario)
-
-module.exports = router
