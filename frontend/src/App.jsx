1eb1dd9566954f8ff550f794e0c927ed5f3afd74
Edu Moreno <e.m.morenolp@gmail.com>
2024-05-30 16:01:09 -0300

    login frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 // import './App.css'
-import AppLayout from './layouts/AppLayout'
-import AuthLayout from "./layouts/AuthLayout"
-import Register from './pages/register/Register'
+import AppLayout from "./layouts/AppLayout";
+import AuthLayout from "./layouts/AuthLayout";
+import Register from "./pages/register/Register";
+import Login from "./pages/register/Login";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<AppLayout />}>
-          <Route path='/admin/register' element={<Register />} index/>
+          <Route path="/admin/register" element={<Register />} index />
+          <Route path="/login" element={<Login />} />
         </Route>
 
-        <Route element={<AuthLayout />}>
-
-        </Route>
+        <Route element={<AuthLayout />}></Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
