abdad9b1551cb960bca1aa6d14e0e31f9b18f058
jlcapoterojas <70924972+jlcapor@users.noreply.github.com>
2024-05-31 02:34:14 -0500

    Registro terminado

--- backend/src/handlers/User/postUser.js
+++ backend/src/handlers/User/postUser.js
@@ -6,9 +6,6 @@ const createUser = async(req, res) =>{
     const responseRegister = await userPost(email, password)
     res.status(200).json(responseRegister);
   } catch (error) {
-    console-console.log('====================================');
-    console.log(error);
-    console.log('====================================');
     res.status(401).json({ error: error.message });
   }
 
